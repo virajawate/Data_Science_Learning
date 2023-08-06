eca942b988f47c750a29fa88cd4cff65cfc6c243
virajawate <viraj.awate@gmail.com>
2023-08-07 01:29:35 +0530

    Flask Dir change

--- Flask_wth_json/app.py
+++ Flask_wth_json/app.py
@@ -1,18 +1,21 @@
 from flask import Flask, request, render_template, jsonify, redirect, url_for
 import json
+import os
 app = Flask(__name__)
 
 
 @app.route('/')
 def welcome():
+    print(os.getcwd())
     return render_template("Welcome.html")
 
 
 @app.route('/calculator', methods=['GET', 'POST'])
 def operations():
     if request.method == 'GET':
+        
         try:
-            with open('flask/templates/Calculator.json', 'r') as json_form:
+            with open('templates/Calculator.json', 'r') as json_form:
                 form_data = json.load(json_form)
                 print("form rendered")
             return render_template("Calculator.html", form_data=form_data)
@@ -45,7 +48,7 @@ def operations():
             
             result_data = {"result" : result}
             print("Calculated Result:", result)  
-            with open('flask/templates/Result.json', 'w') as result_file:
+            with open('templates/Result.json', 'w') as result_file:
                 json.dump(result_data, result_file)
                 print("Saved Result Data:", result_data)
             return redirect(url_for('final'))
@@ -55,7 +58,7 @@ def operations():
 @app.route('/result')
 def final():
     try:
-        with open('flask/templates/Result.json', 'r') as result_file:
+        with open('templates/Result.json', 'r') as result_file:
             result_data = json.load(result_file)
             print("Retrieved Result Data:", result_data)  # Debugging
             result = result_data["result"]
