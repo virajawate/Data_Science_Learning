daf1cf8379877ecac463018433b4d8abdf35df92
virajawate <viraj.awate@gmail.com>
2023-11-22 23:48:46 +0530

    MLops_initial

---
+++ End2End_Sandbox/src/DiamondPricePrediction/components/data_ingestion.py
@@ -0,0 +1,12 @@
+'''
+Code for modularising data ingestion.
+'''
+
+class Daa_Ingestion:
+    def __init__(self):
+        pass
+    
+    def init_data_ingestion(self):
+        pass
+    
+    
---
+++ End2End_Sandbox/src/DiamondPricePrediction/components/data_transformation.py
@@ -0,0 +1,12 @@
+'''
+Code for pre-processing of data
+'''
+
+class Data_Transformation:
+    def __init__(self):
+        pass
+    
+    def get_data_transformation(self):
+        pass
+    
+    
