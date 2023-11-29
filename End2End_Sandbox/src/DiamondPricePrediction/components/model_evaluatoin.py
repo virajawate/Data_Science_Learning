10661c1dd1243f93181df681a8092d9fd88f4348
virajawate <viraj.awate@gmail.com>
2023-11-30 01:06:24 +0530

    ModelProcessingFiles

---
+++ End2End_Sandbox/src/DiamondPricePrediction/components/model_evaluatoin.py
@@ -0,0 +1,47 @@
+import os, sys
+import numpy as np
+import pickle
+import mlflow
+import mlflow.sklearn
+from urllib.parse import urlparse
+from src.DiamondPricePrediction.utils.utils import load_object
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+
+class ModleEvaluation:
+    def __init__(self) -> None:
+        pass
+    
+    def eval_metrics(self, actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        
+        return rmse, mae, r2
+    
+    def initiate_model_evaluation(self, train_a, test_a):
+        try:
+            x_test, y_test = (test_a[:, :-1], test_a[:, -1])
+            
+            model_path = os.path.join("artifacts", "model.pkl")
+            model = load_object(model_path)
+            
+            mlflow.set_registry_uri("")
+            
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+            print(tracking_url_type_store)
+            
+            with mlflow.start_run():
+                y_pred = model.predict(x_test)
+                (rmse, mae, r2) = self.eval_metrics(y_test, y_pred)
+                
+                mlflow.log_metric('rmse', rmse)
+                mlflow.log_metric('mae', mae)
+                mlflow.log_metric('r2', r2)
+                
+                if tracking_url_type_store != 'file':
+                    mlflow.sklearn.log_model(model, "model", registered_model_name='ml_model')
+                else:
+                    mlflow.sklearn.log_model(model, 'model')
+        except Exception as e:
+            raise e
---
+++ End2End_Sandbox/src/DiamondPricePrediction/components/model_trainer.py
@@ -0,0 +1,47 @@
+import os, sys
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass
+from src.DiamondPricePrediction.logger import logging
+from src.DiamondPricePrediction.exception import CustomException
+from src.DiamondPricePrediction.utils.utils import logging
+from src.DiamondPricePrediction.utils.utils import Evaluate_model
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_path = os.path.join("artifacts", "model.pkl")
+
+
+class ModelTrainer:
+    def __init__(self) -> None:
+        self.model_trainer_config = ModelTrainerConfig()
+    
+    def initate_model_training(self, train_arr, test_arr):
+        try:
+            logging.info("Splitting Dependent and Independent variables from train and test data...")
+            x_train, y_train, x_test, y_test = (
+                train_arr[:, :-1], train_arr[:, -1],
+                test_arr[:, :-1], test_arr[:, -1])
+            models = {'Lasso':Lasso(), 'Ridge':Ridge(),
+                'LinearResgression':LinearRegression(),
+                'Elasticnet':ElasticNet()}
+            model_report:dict = Evaluate_model(x_train, y_train, x_test, y_test)
+            print(model_report)
+            print('\n==================================================================================\n')
+            logging.info(f'Model Report : {model_report}')
+            best_model_score = max(sorted(model_report.values()))
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            best_model = models[best_model_name]
+            print(f"Best Model Found, Model Name : {best_model_name}, R2 Score : {best_model_score}")
+            print('\n===================================================================================\n')
+            logging.info(f"Best Model Found, Model Name : {best_model_name}, R2 Score : {best_model_score}")
+            
+            save_object(
+                file_path = self.model_trainer_config.trained_model_path,
+                obj = best_model
+            )
+            
+        except Exception as e:
+            logging.info('Exception occured at Model Training....')
+            raise CustomException(e, sys)
